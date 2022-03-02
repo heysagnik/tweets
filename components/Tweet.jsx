863e0d7290dd893b5f2fe4a4166c1b2d2f6e4158
Sagnik Sahoo <sahoosagnik1@gmail.com>
2022-03-03 00:13:41 +0530

    Update Tweet.jsx

--- components/Tweet.jsx
+++ components/Tweet.jsx
@@ -86,11 +86,11 @@ const Tweet = ({tweet, showTime, showMetrics, showSource, showImage}) => {
 	            {showMetrics && <div className='metrics'>
                 	<Text fontSize={font_size_small}>
                 		<span className='bold'>{modLikes}</span> 
-                                <span className='seclikes'> 
-                                <svg style={{color: '#E81C4F'}} viewBox="0 0 24 24" width="16" fill="currentColor" focusable="false">
+                                <div className='seclikes'> 
+                                <svg style={{color: '#E81C4F'}} viewBox="0 0 54 72" width="16" fill="currentColor" focusable="false">
                                 <path d="M38.723,12c-7.187,0-11.16,7.306-11.723,8.131C26.437,19.306,22.504,12,15.277,12C8.791,12,3.533,18.163,3.533,24.647 C3.533,39.964,21.891,55.907,27,56c5.109-0.093,23.467-16.036,23.467-31.353C50.467,18.163,45.209,12,38.723,12z"/>
                                 </svg>
-                                </span>  <span className='bold'>{modRetweets}</span> <span className='sec'> Retweets</span>
+                                </Div>  <span className='bold'>{modRetweets}</span> <span className='sec'> Retweets</span>
                 	</Text>
 	            </div>}
 	        </div>
