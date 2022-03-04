d6ae8c4b68f5996c947ac1aaf3d532b30ecee0de
Sagnik Sahoo <sahoosagnik1@gmail.com>
2022-03-04 12:30:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -120,14 +120,14 @@ function App()
             </Flex>
 
             <footer>
-                <center>
+                <Box>
                 <HStack inline>
                 <Box className='i'>
                  <Text px='1rem' className='i' color='gray.500' fontSize='lg'>Made with </Text></Box>
                  <Box class="container2"><Box class="heart"></Box></Box>
                  <Box><Text px='1rem' className='i' color='gray.500' fontSize='lg'> by 
                  <Link href="https://www.twitter.com/heysagnik" rel="noreferrer" target='_blank'isExternal> Sagnik</Link>.</Text>
-                 </Box></HStack></center>
+                 </Box></HStack></Box>
             </footer>
             
       </Box>
